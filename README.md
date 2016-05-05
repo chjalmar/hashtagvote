a0866d4c56630cfc053e2c881386a6e6fc3d4246
the_input <chjalmar@gmail.com>
2016-05-05 17:40:22 -0300

    Agregado script htv2, que usa la librería 'twitter' en lugar de 'twit'

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 #Sinopsis
 App en Node para escuchar streams de Twitter buscando ciertos hashtags, y escribiendo los resultados en JSON
+
+#Observaciones
+
+La propiedad 'language', que se puede pasar al método stream (aquí se está haciendo), tiene un problema: el idioma ('data.lang') es auto-detectado por Twitter, y cuando un tweet está muy mal escrito, o todas las palabras son hashtags, Twitter es incapaz de detectar el idioma, devolviendo el valor 'und' (undefined) para la propiedad 'lang'. Entonces, ese tweet no será incluido en el stream
+
+#Cambios
+
+Agregado htv2.js, que utiliza la librería 'twitter' en lugar de 'twit'. Cambios en esa versión:
+
+- La librería 'twitter' recibe la credencial de 'access_token' como 'access_token_key'.
+- Usando la librería 'twitter', la propiedad 'track' del objeto que se pasa como segundo parámetro al método 'stream' sólo recibe un string con palabras, etiquetas o nombres de usuario separados por comas (la coma es como un operador lógico "OR": ver documentación de la API de Twitter en https://dev.twitter.com/streaming/overview/request-parameters#track ); mientras que la librería 'twit' puede recibir un arreglo, y convertirá los elementos del arreglo en una lista de elementos separados por comas.
+- En la librería 'twitter', el stream recibido pasará como parámetro a la función callback que va como tercer parámetro del método 'stream'. Dentro del callback, se puede llamar el método 'on' para manejo de errores, entre otras opciones.
---
+++ htv2.js
@@ -0,0 +1,85 @@
+var Twit = require('twitter');
+var fs = require('fs');
+var credentials = require("./creds.js");
+//agregue las credenciales de su cuenta Twitter
+
+var credenciales = credentials.creds('twitter');
+
+console.log(credenciales);
+
+var T = new Twit(credenciales);
+
+//Con la librería 'twitter' el método stream no recibe un array para track en el 2do parámetro,
+//sino un string, con palabras o hashtags o usuarios separados por comas, la como es como un "OR"
+T.stream('statuses/filter', { track: '#FelizJueves,#siguemeytesigo' }, function(stream){
+
+  stream.on('data', function (tweet) {
+    //En esta función quiero:
+    //Cuando aparezca un nuevo tweet (que use los hashtags del concurso):
+    //- Guardarlo en la Base de Datos
+    //- Escribir en un archivo de texto un objeto con:
+    //  - La cuenta total que participaciones o tweets válidos que llevamos (traerla de la Base de datos)
+    //  - Los datos del último Tweet: usuario, texto del tweet, timestamp
+    
+    console.log(tweet.user.screen_name);
+    console.log(tweet.text);
+    console.log(tweet.entities.hashtags);  
+    console.log("\n");
+    
+    //arreglo "opciones", de opciones a votar en el concurso
+    //OJO: Escribir en el arreglo el hashtag sin acentos y sin el numeral (#); las mayúsculas no importan
+    var opciones = ["Geografia","MichelOnfray"];
+    var hashtags = tweet.entities.hashtags;
+    var contenido;
+    
+    //recorrer las opciones para ver por cuál votó el tweet
+    for (var i = 0; i < opciones.length; i++) {
+    	//recorrer los hashtags del tweet para compararlos con las opciones
+      var contvotos = 0;
+      for (var hashtag in hashtags) {
+      	//eliminar acentos si el usuario los introdujo en el hashtag
+      	var opcion = hashtags[hashtag].text;
+      	opcion = opcion.replace(/á/ig,"a");
+        opcion = opcion.replace(/é/ig,"e");
+        opcion = opcion.replace(/í/ig,"i");
+        opcion = opcion.replace(/ó/ig,"o");
+        opcion = opcion.replace(/Ú/ig,"u");
+        //poner todo en minúscula y comparar cada hashtag del tweet con nuestras opciones participantes
+        //console.log(opcion.toLowerCase() + "||||" + opciones[i].toLowerCase());
+        if (contvotos === 0) {
+          //seguir chequeando sólo si no ha votado por alguna de las opciones en el mismo tweet
+          if (opcion.toLowerCase() === opciones[i].toLowerCase()) {
+             contenido = {
+                           totalcount: 123,
+                           latest: {  created_at: tweet.created_at, 
+    	                               screen_name: tweet.user.screen_name, 
+    	                                      text: tweet.text,
+    	                                      voto: opciones[i]
+    	                             }
+                         };     	
+             //Escribir al archivo
+             printfile(contenido); 
+          	 console.log("Ultimo tweet agregado al archivo, por @"+ tweet.user.screen_name +".");
+          	 i = opciones.length - 1;
+          	 contvotos++;
+          }
+        }
+      }  
+    }
+        
+  });
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
+
+function printfile(contenido){
+    //fs.writeFile("latest.json", JSON.stringify(contenido,null,4), function(err) {
+    //ahora mismo estoy acumulando los tweets que cumplen los criterios en vez de guardar sólo el último
+    fs.writeFile("latest.json", JSON.stringify(contenido,null,4) + "\n",{ flag:"a" }, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+    });
+    
+}
